852bfef574955fa01bdd0566fe6c8a1ba9840b01
hebangwen <1925585604@qq.com>
2024-09-27 23:26:40 +0800

    refactor(prog1): split workload into smaller chunk

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -29,22 +29,23 @@ extern void mandelbrotSerial(
 // Thread entrypoint.
 void workerThreadStart(WorkerArgs * const args) {
 
+    // auto start = CycleTimer::currentSeconds();
+
     // TODO FOR CS149 STUDENTS: Implement the body of the worker
     // thread here. Each thread should make a call to mandelbrotSerial()
     // to compute a part of the output image.  For example, in a
     // program that uses two threads, thread 0 could compute the top
     // half of the image and thread 1 could compute the bottom half.
 
-    int numRows = args->height / args->numThreads;
-    int startRows = args->threadId * numRows;
-    if (args->threadId == args->numThreads - 1) {
-        numRows = args->height - startRows;
+    for (int i = args->threadId; i < args->height; i += args->numThreads) {
+        mandelbrotSerial(args->x0, args->y0, args->x1, args->y1,
+                         args->width, args->height,
+                         i, 1,
+                         args->maxIterations, args->output);
     }
 
-    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1,
-                     args->width, args->height,
-                     startRows, numRows,
-                     args->maxIterations, args->output);
+    // auto end = CycleTimer::currentSeconds();
+    // printf("Thread %d: %lf ms\n", args->threadId, (end - start) * 1000);
 }
 
 //
